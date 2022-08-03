f17c43ba75fd78b334e8c4a5565f279c65730e8a
eliott <eliottbidault@gmail.com>
2022-08-03 15:29:01 +0200

    added the links animation

--- script/nav.js
+++ script/nav.js
@@ -1,8 +1,27 @@
+let links = document.getElementsByClassName('navigation-links');
+
 //making the nav appears
 document.getElementsByClassName('burger')[0].addEventListener('click', () => {
     document.getElementsByTagName('nav')[0].style.cssText = "transform: scaleX(1);";
-    //making the links appear (TO DO)
+
+    let delay = 0;
+    setTimeout(() => {
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.cssText = `transition-delay: ${delay}s; transform: skewY(0deg); opacity: 1;`;
+            delay += 0.15;
+        }
+    }, 1250 + 150);
 });
+
 document.getElementsByClassName('x')[0].addEventListener('click', () => {
-    document.getElementsByTagName('nav')[0].style.cssText = "transform: scaleX(0);";
+    let delay = 0;
+    for (let i = 0; i < links.length; i++) {
+        links[i].style.cssText = `transition-delay: ${delay}s; transform: skewY(10deg); opacity: 0;`;
+        delay += 0.15;
+    }
+
+    setTimeout(function() {
+        document.getElementsByTagName('nav')[0].style.cssText = "transform: scaleX(0);";
+        console.log((0.15 * links.length) * 10, links.length);
+    }, (150 * links.length) + 300);
 });
