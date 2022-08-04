2a57bad127582b8dbd04b51dbf3ee5094727cf4e
eliott <eliottbidault@gmail.com>
2022-08-04 22:52:45 +0200

    make the picture follow the cursor and change links colors

--- script/nav.js
+++ script/nav.js
@@ -20,8 +20,53 @@ document.getElementsByClassName('x')[0].addEventListener('click', () => {
         delay += 0.15;
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementsByTagName('nav')[0].style.cssText = "transform: scaleX(0);";
         console.log((0.15 * links.length) * 10, links.length);
     }, (150 * links.length) + 150);
+});
+
+//creating the picture chang animation
+for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', function() {
+        let catergoryIndex = this.index;
+    });
+}
+
+//making the pictures follow the cursor
+//https://www.superhi.com/video/smooth-movements-with-javascript
+
+const image = document.getElementsByClassName("link-image-container")[0];
+
+let mouseX = 0;
+let mouseY = 0;
+
+let imageX = 0;
+let imageY = 0;
+
+let speed = 0.25;
+
+
+function movePicture() {
+
+    let distX = mouseX - imageX;
+    let distY = mouseY - imageY;
+
+
+    imageX = imageX + (distX * speed);
+    imageY = imageY + (distY * speed);
+
+    let centerX = Math.max(Math.min(imageX - (image.offsetWidth / 2), window.innerWidth - image.offsetWidth), 0);
+    let centerY = Math.max(Math.min(imageY - (image.offsetHeight / 2), window.innerHeight - image.offsetHeight), 0);
+
+    image.style.left = `${centerX}px`;
+    image.style.top = `${centerY}px`;
+
+    requestAnimationFrame(movePicture);
+}
+movePicture();
+
+document.addEventListener("mousemove", function (event) {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
 });
